149e77c339a670e9acd84758e5ca1d81c4e1982a
Simon Urzua <simon.urzua@rover.com>
2020-05-26 09:36:25 -0500

    Version 0.2.4

--- Alooma-iOS/Alooma.m
+++ Alooma-iOS/Alooma.m
@@ -18,7 +18,7 @@
 #import "AloomaLogger.h"
 #import "NSData+AloomaBase64.h"
 
-#define VERSION @"0.1.1"
+#define VERSION @"0.2.4"
 
 static NSString * const kSendingTimePlaceHolder = @"<SendingTimePlaceHolder>";
 static NSString * const kSendingTimeKey = @"sending_time";
